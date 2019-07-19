9f1cb8dc6e1ea185f86cce339b70abda621e9a21
mien <mienth2@gmail.com>
2019-07-19 20:36:49 +0200

    fixing typo

--- mksymlinks.sh
+++ mksymlinks.sh
@@ -3,12 +3,12 @@
 ln -s ~/Repositories/dotfiles/dmrc ~/.dmrc
 ln -s ~/Repositories/dotfiles/zshrc ~/.zshrc
 
-if [ -d ! ~/.zsh ] 
+if [ ! -d ~/.zsh ] 
 then 
     ln -s ~/Repositories/dotfiles/zsh ~/.zsh
 fi
 
-if [ -d ! ~/.config ] 
+if [ ! -d ~/.config ] 
 then 
     ln -s ~/Repositories/dotfiles/config ~/.config
 fi
