6d827726b3591065dfa1f184464937baf7731c47
mien <mienth2@gmail.com>
2019-07-20 22:36:03 +0200

    add vimrc

--- mksymlinks.sh
+++ mksymlinks.sh
@@ -2,13 +2,12 @@
 
 ln -s ~/Repositories/dotfiles/dmrc ~/.dmrc
 ln -s ~/Repositories/dotfiles/zshrc ~/.zshrc
+ln -s ~/Repositories/dotfiles/zshrc ~/.vimrc
 
-if [ ! -d ~/.zsh ] 
-then 
+if [ ! -d ~/.zsh ] then 
     ln -s ~/Repositories/dotfiles/zsh ~/.zsh
 fi
 
-if [ ! -d ~/.config ] 
-then 
+if [ ! -d ~/.config ] then 
     ln -s ~/Repositories/dotfiles/config ~/.config
 fi
