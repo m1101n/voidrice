0863bac65d8b067e9cf8231091fe8dbc8527dc52
mien <mien@void.fritz.box>
2019-07-21 15:38:30 +0200

    edit

--- mksymlinks.sh
+++ mksymlinks.sh
@@ -2,7 +2,7 @@
 
 ln -s ~/Repositories/dotfiles/dmrc ~/.dmrc
 ln -s ~/Repositories/dotfiles/zshrc ~/.zshrc
-ln -s ~/Repositories/dotfiles/zshrc ~/.vimrc
+ln -s ~/Repositories/dotfiles/vimrc ~/.vimrc
 
 if [ ! -d ~/.zsh ] then 
     ln -s ~/Repositories/dotfiles/zsh ~/.zsh
