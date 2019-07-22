9f6891a753d3d791afa7e47c5214e9a9172b7883
Your Name <you@example.com>
2019-07-22 18:02:17 +0200

    update

--- mksymlinks.sh
+++ mksymlinks.sh
@@ -5,14 +5,17 @@ ln -s ~/Repositories/dotfiles/zshrc ~/.zshrc
 ln -s ~/Repositories/dotfiles/vimrc ~/.vimrc
 ln -s ~/Repositories/dotfiles/Xresources ~/.Xresources
 
-if [ ! -d ~/.zsh ] then 
+if [ ! -d ~/.zsh ] 
+then 
     ln -s ~/Repositories/dotfiles/zsh ~/.zsh
 fi
 
-if [ ! -d ~/.config ] then 
+if [ ! -d ~/.config ] 
+then 
     ln -s ~/Repositories/dotfiles/config ~/.config
 fi
 
-if [ ! -d ~/.scripts ] then 
-    ln -s ~/Repositories/dotfiles/scripts ~/.scripts
+if [ ! -d ~/.local/bin ] 
+then 
+    ln -s ~/Repositories/dotfiles/bin ~/.local/bin
 fi
