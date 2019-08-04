07788e5078133a5154d499df28cb50d57228e0c5
mien <mien@void.fritz.box>
2019-08-04 10:10:14 +0200

    update

--- mksymlinks.sh
+++ mksymlinks.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 
-ln -s $HOME/Repositories/dotfiles/dmrc ~/.dmrc
+#########
+# $Home #
+#########
 ln -s $HOME/Repositories/dotfiles/zshrc ~/.zshrc
 ln -s $HOME/Repositories/dotfiles/vimrc ~/.vimrc
 ln -s $HOME/Repositories/dotfiles/xprofile ~/.xprofile
 ln -s $HOME/Repositories/dotfiles/Xresources ~/.Xresources
 
-cp $HOME/Repositories/dotfiles/fonts/icomoon.ttf /usr/share/fonts/TTF/
-
+##########
+## .zsh ##
+##########
 if [ ! -d ~/.zsh ] 
 then 
     ln -s ~/Repositories/dotfiles/zsh ~/.zsh
 fi
 
-if [ ! -d ~/.config ] 
-then 
-    ln -s ~/Repositories/dotfiles/config ~/.config
-fi
-
+##########
+# .local #
+##########
 if [ ! -d ~/.local/bin ] 
 then 
     ln -s ~/Repositories/dotfiles/bin ~/.local/bin
 fi
+
+###########
+# .config #
+###########
+if [ ! -d ~/.config ] 
+then 
+	mkdir $HOME/.config
+fi
+configs=( alacritty bspwm compton dunst nvim polybar sxhkd )
+for conf in "${configs[@]}"
+do 
+	if [ ! -d ~/.config/$conf ] 
+	then 
+    	ln -s ~/Repositories/dotfiles/config/$conf ~/.config/$conf
+	fi
+done
